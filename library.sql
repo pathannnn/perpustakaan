805fd660c0f9818ffcfbfcfc390cea423bef820e
Fatih Masrukhan <111848439+pathannnn@users.noreply.github.com>
2024-10-12 22:07:21 +0700

    sql

---
+++ library.sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 12, 2024 at 05:07 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_lengkap`, `username`, `password`) VALUES
+(1, 'Fatih Masrukhan', 'pathannnn', '$2y$10$J2U/E49j2TNaa08xsqo0GOrDS3vNkL3LFe0u7MQWBiW7ci9nsBRiG'),
+(3, 'Lulu', 'LL', '$2y$10$.euChIyEok/0JGufZJhT3epu6JhQT4Ycb0lHhfnyRwS9RIg8UUJnW'),
+(4, 'Nayla', 'adek', '$2y$10$0JaTR6T5oLsSrgkYZJO6GO4Q.Nbit7eXnim2cgqZ72q8PMvoIIz8S');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` varchar(5) NOT NULL,
+  `isbn` varchar(30) DEFAULT NULL,
+  `judul` varchar(255) DEFAULT NULL,
+  `penulis` varchar(11) DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `tahun_terbit` varchar(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `isbn`, `judul`, `penulis`, `id_kategori`, `tahun_terbit`) VALUES
+('', '35325452', 'Laskar Pelangi', 'Andrea Hira', 4, '2005'),
+('N-001', '5424535', 'Bumi Manusia', 'Pramoedya A', 1, '1999');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_peminjaman`
+--
+
+CREATE TABLE `detail_peminjaman` (
+  `id_peminjaman` varchar(6) DEFAULT NULL,
+  `id_buku` varchar(5) DEFAULT NULL,
+  `jumlah_pinjam` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(21) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Ilmiah'),
+(3, 'Peternakan'),
+(4, 'Fiksi'),
+(5, 'Novel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` varchar(6) NOT NULL,
+  `nisn` varchar(20) DEFAULT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `tg_harus_kembali` date DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `status_pinjam` enum('Pinjam','Kembali') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `nama_penerbit` varchar(50) DEFAULT NULL,
+  `kota` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengadaan`
+--
+
+CREATE TABLE `pengadaan` (
+  `id_pengadaan` varchar(6) NOT NULL,
+  `tgl_pengadaan` date DEFAULT NULL,
+  `id_buku` varchar(5) DEFAULT NULL,
+  `asal_buku` varchar(100) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_kembali` varchar(6) NOT NULL,
+  `id_pinjam` varchar(6) DEFAULT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `denda` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penulis`
+--
+
+CREATE TABLE `penulis` (
+  `id_penulis` int(11) NOT NULL,
+  `nama_penulis` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` varchar(20) NOT NULL,
+  `nama_siswa` varchar(100) DEFAULT NULL,
+  `jkel` char(1) DEFAULT NULL,
+  `tempat_lahir` varchar(30) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(13) DEFAULT NULL,
+  `foto_siswa` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_peminjaman`
+--
+
+CREATE TABLE `temp_peminjaman` (
+  `id_temp` int(11) NOT NULL,
+  `id_buku` varchar(5) DEFAULT NULL,
+  `jumlah_pinjam` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indexes for table `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indexes for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  ADD PRIMARY KEY (`id_pengadaan`);
+
+--
+-- Indexes for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_kembali`);
+
+--
+-- Indexes for table `penulis`
+--
+ALTER TABLE `penulis`
+  ADD PRIMARY KEY (`id_penulis`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`);
+
+--
+-- Indexes for table `temp_peminjaman`
+--
+ALTER TABLE `temp_peminjaman`
+  ADD PRIMARY KEY (`id_temp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `penulis`
+--
+ALTER TABLE `penulis`
+  MODIFY `id_penulis` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
